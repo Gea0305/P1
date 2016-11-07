f944008318c8546e957f007b7f6c603a8b81bc49
Gonzalo Lencina <100317214@alumnos.uc3m.es>
2016-11-07 14:35:31 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,29 +7,32 @@
 #include <cstring>
 using namespace std;
 
-int main(){
-
-
+ 	
+	int HEIGHT;
+	int WIDTH;
+	
+int main(int argc, char *argv[]){
+ //argc= numero de argumentos especificados por linea de comandos
+//argv[i] contiene el contenido del argumento i
   
   
-  
+  /*
 
 	ifstream inFile;
 	size_t size = 0; 
 
-	inFile.open( "hola.txt", ios::in|ios::binary|ios::ate );
+	inFile.open( "imagen.txt", ios::in | ios::binary | ios::ate );
 	 if (inFile.is_open()) {
 	char* oData = 0;
 	
 	//inFile.seekg(0, ios::end); // set the pointer to the end
 	size = inFile.tellg() ; // get the length of the file
 	cout << "Size of file: " << size<<endl;
-	//Ponemos el puntero en la primera posicion
 	inFile.seekg(0); // set the pointer to the beginning
 
 	oData = new char[ size ]; 
 	inFile.read( oData, 5 );
-	
+
 	
 	cout<<oData[0];
 	cout<<oData[1];
@@ -42,8 +45,49 @@ int main(){
 	cerr << "Error opening file";
 }
 	return 0;
-
+*/
 return 0;
 }
+void leer_dimensiones(const char* fileName){
+	
+	ifstream pInFile;
+	pInFile.open(fileName, ios::in | ios::binary); // open fileName and read as binary.
+    
+    if (pInFile.is_open()) {
+    pInFile.seekg(0, ios::beg); //pos filter at beginning of image file.
+	char heightData[4]; 
+	char widthData[4]; 
+	pInFile.read(heightData, 4); //Lees la altura
+	
+	/*Calculamos la altura y lo metemos en height*/
+	
+	pInFile.read(widthData, 4); //Lees la anchura
+	
+	/*Calculas la anchura y lo metes en width*/
+	
+}else{
+	cerr <<"Error opening file";
+}
+	
+}
+
+void aplicar_mascara(){
+		
+		/* 	1)Leer 1byte, que son dos posiciones del fichero
+		   	2)Leet 1 byte, del fichero de la mascara, que serian dos digitos, y pasarlo a decimal. 01=1, 00=0
+			3)Multiplicar el primero numero, por el 0 o el 1 de la mascara segun corresponda
+			4)Escribir el resultado en un fichero de salida llamado mask_out.img
+	*/
+	
+}
+
+	
+	
+	
+	
+	
+	
+
+
 
 

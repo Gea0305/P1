2dcb5d249261f533b9087e8affe75f03c87efdee
papitomirey <100315058@alumnos.uc3m.es>
2016-11-07 15:52:31 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,38 +17,10 @@ int main(/*int argc, char *argv[]*/){
 //argv[i] contiene el contenido del argumento i
   leer_dimensiones();
   
-  /*
-
-	ifstream inFile;
-	size_t size = 0; 
-
-	inFile.open( "imagen.txt", ios::in | ios::binary | ios::ate );
-	 if (inFile.is_open()) {
-	char* oData = 0;
-	
-	//inFile.seekg(0, ios::end); // set the pointer to the end
-	size = inFile.tellg() ; // get the length of the file
-	cout << "Size of file: " << size<<endl;
-	inFile.seekg(0); // set the pointer to the beginning
-
-	oData = new char[ size ]; 
-	inFile.read( oData, 5 );
-
-	
-	cout<<oData[0];
-	cout<<oData[1];
-	cout<<oData[2];
-	cout<<oData[3];
-	cout<<oData[4];
-	
-	//getchar();
-}else{
-	cerr << "Error opening file";
-}
-	return 0;
-*/
+  
 return 0;
 }
+
 void leer_dimensiones(/*const char* fileName*/){
 	
 	ifstream pInFile;

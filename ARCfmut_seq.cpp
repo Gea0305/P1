6d1b233cd52b4e991f9a9438428ec9624f2dba37
Gonzalo Lencina <100317214@alumnos.uc3m.es>
2016-11-18 20:54:26 +0100

    Update ARCfmut_seq.cpp

--- ARCfmut_seq.cpp
+++ ARCfmut_seq.cpp
@@ -264,57 +264,47 @@ void rotacion(string ImageFile, string OutputFile, double gr){
        	if(!pOutFile) { 
     		cout << "Error al abrir el fichero "<<OutputFile<<" para escribir"<<endl;  
    		} 
-   		cout<<"Escribo la cabecera en el archivo de rotacion"<<endl;
-		unsigned char imgdata; 
-		//Escribimos en el rot_out, los primeros 8 bytes de su tamaÃ±o, que sera el tamaÃ±o de la imagen de entrada
-	   	unsigned char heightData[4]; 
-		unsigned char widthData[4]; 
-		InFile.seekg(0, ios::beg); //pos filter at beginning of image file.
-		//Leo los 4 primeros bytes
-		InFile.read( (char *)& heightData, 4 ); //Lees la altura
-		InFile.read( (char *)& widthData, 4); 
-		//Escribo en el fichero de salida, los tamaÃ±os de la matriz
-		pOutFile.write( (char *)& heightData, 4);
-		pOutFile.write( (char *)& widthData, 4);
-		cout<< "He escrito la cabecera y voy con lo siguiente"<<endl;
-		double xc , yc , xi,yi;
+   		//Se escribe la cabecera 
+   		unsigned char cabecera[8];
+   		InFile.read((char*)& cabecera, 8);
+   		pOutFile.write( (char *)& cabecera, 8);	
+   		//Se vuelca la matriz en el vector
+		vector<unsigned char> imgdata(matrix_size); 
+			for (int i=0; i<(matrix_size); ++i){
+				InFile.read((char*)& imgdata[i], 1);
+			}
+			InFile.close();
+		vector<unsigned char> fin(WIDTH*HEIGHT);;
+		double xc,yc,xi,yi;
 		int xf,yf;
-		vector<unsigned char> fin(WIDTH*HEIGHT, 0); //Vector con los tramos inicializado a 0
+		
 		//Calculamos el centro de la imagen
-		cout<<"Voy a calcular el centro"<<endl;
 		xc=WIDTH/2;
 		yc=HEIGHT/2;
-		cout<<"El centro es xc:"<<xc<<" yc:"<<yc<<endl;
 		int num_colores= 0;
-		int i,j;
 		int contador=0;
 		while(num_colores<3){
-			for (j=0; j<HEIGHT; j++){
-				for(i=0; i<WIDTH; i++){
-
-					for (; contador<WIDTH*HEIGHT; contador++){
-				
+			for (int j=0; j<HEIGHT; j++){
+				for(int i=0; i<WIDTH; i++){
+					
 					xi=i-xc;
 					yi=j-yc;
 					xf= ceil( cos((gr*M_PI)/180)*xi - sin((gr*M_PI)/180)*yi +xc);
 					yf= ceil( sin((gr*M_PI)/180)*xi + cos((gr*M_PI)/180)*yi +yc);
-					InFile.read( (char *)& imgdata,1);
+					
 					if(yf<HEIGHT && yf>=0 && xf<WIDTH && xf>=0){
-						fin[contador]= imgdata;
+						fin[yf*WIDTH + xf]= imgdata[contador];
+						
 					}
-					
-				}
+				contador++;	
 				}
 			}
 			
-			
-			for (contador=0; contador<WIDTH*HEIGHT; contador++){
-					
-					pOutFile.write( (char *)& fin[contador], 1);
-				
-			}
+				for (int i=0; i<WIDTH*HEIGHT; i++){
+					pOutFile.write( (char *)& fin[i], 1);
+					}
 			num_colores++;
-			memset(&fin[0], 0, fin.size() * sizeof fin[0]);
+			memset(&fin[0], 0, WIDTH*HEIGHT);
 		}
 	}else{
 		cerr<<"Error al abrir "<<ImageFile<<endl;

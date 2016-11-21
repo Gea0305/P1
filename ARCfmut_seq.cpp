97face49574a0db72b80bd06cae1c0e006dcc9fa
Gonzalo Lencina <100317214@alumnos.uc3m.es>
2016-11-21 23:34:31 +0100

    Comentado de codigo

--- ARCfmut_seq.cpp
+++ ARCfmut_seq.cpp
@@ -153,23 +153,20 @@ int main(int argc, char ** argv){
 void leer_dimensiones(string fileName){
 	ifstream InFile;
 	InFile.open(fileName, ios::in | ios::binary); 
-    if (InFile.is_open()) {
-	    //Donde almacenamos los datos tienen que ser unsigned char, ya que van desde 0 a 255    
+    if (InFile.is_open()) {   
 		unsigned char heightData[4]; 
 		unsigned char widthData[4]; 
 		InFile.seekg(0, ios::beg);  
-		//Leo los 4 primeros bytes
-		InFile.read( (char *)& heightData, 4 ); 
-		HEIGHT += (int)heightData[0] | ((int)heightData[1]<<8) | ((int)heightData[2]<<16) | ((int)heightData[3]<<24);
-		InFile.read( (char *)& widthData, 4); 
-		WIDTH += (int)widthData[0] | ((int)widthData[1]<<8) | ((int)widthData[2]<<16) | ((int)widthData[3]<<24);
-		matrix_size=  (HEIGHT*WIDTH)*3;
+		InFile.read( (char *)& heightData, 4 ); //Se leen los bytes de la altura
+		HEIGHT += (int)heightData[0] | ((int)heightData[1]<<8) | ((int)heightData[2]<<16) | ((int)heightData[3]<<24); //Se convierten de little endian a big endian
+		InFile.read( (char *)& widthData, 4); //Se leen los bytes de la anchura
+		WIDTH += (int)widthData[0] | ((int)widthData[1]<<8) | ((int)widthData[2]<<16) | ((int)widthData[3]<<24); //Se convierten de little endian a big endian
+		matrix_size=  (HEIGHT*WIDTH)*3; //El tamaño de la matriz sera de 3 matrices de tamaño HEIGHT*WIDHT, RGB
 		InFile.close();
 	}else{
 		cerr <<"Error opening file";
 	} 
 }
-
 void histograma(string ImageFile, string OutputFile, int t){
 	ifstream InFile;
 	InFile.open(ImageFile, ios::in | ios::binary);
@@ -208,7 +205,6 @@ void histograma(string ImageFile, string OutputFile, int t){
 		cerr<<"Error al abrir el fichero "<<ImageFile<<endl;
 	}
 }
-
 void aplicar_mascara(string ImageFile, string OutputFile, string MaskFile){
 	ifstream InImagen;
 	InImagen.open(ImageFile, ios::in | ios::binary); 
@@ -218,19 +214,19 @@ void aplicar_mascara(string ImageFile, string OutputFile, string MaskFile){
 		if (InMascara.is_open()) {
 	    		streampos fileSize= matrix_size+8;
 			vector<unsigned char> imgdata(fileSize); //Vector para volcar la matriz recibida
-			InImagen.read((char*) &imgdata[0], fileSize);
+			InImagen.read((char*) &imgdata[0], fileSize); //Se leen todos los datos de la imagen
 			InImagen.close();
 			vector<unsigned char> mskdata(fileSize); //Vector para volcar la mascara recibida
-			InMascara.read((char*) &mskdata[0], fileSize);
+			InMascara.read((char*) &mskdata[0], fileSize); //Se leen todos los datos de la mascara
 			InMascara.close();
-			for(int i=8; i<(fileSize); ++i){	//Aplicacion de la mascara
+			for(int i=8; i<(fileSize); ++i){	//Aplicacion de la mascara, sin contar los bytes que especifican las dimensiones
 				imgdata[i]*=mskdata[i];
 			}
           		//Escritura en el fichero de salida
          		ofstream pOutFile;
 			pOutFile.open(OutputFile, ios::out | ios::trunc | ios::binary);	
 	       	if(pOutFile.is_open()) {
-				pOutFile.write((char*)& imgdata[0], fileSize);	
+			pOutFile.write((char*)& imgdata[0], fileSize);	//Se escribe la matriz resultante en el fichero de salida
 	       		pOutFile.close();
    			}else{
     			cerr << "Error al abrir el fichero "<<OutputFile<<" para escribir"<<endl;  
@@ -242,7 +238,6 @@ void aplicar_mascara(string ImageFile, string OutputFile, string MaskFile){
 		cerr<<"Error opening "<<ImageFile<<endl;
 	}
 }
-
 void rotacion(string ImageFile, string OutputFile, double gr){
 	ifstream InFile;
 	InFile.open(ImageFile, ios::in | ios::binary);
@@ -250,32 +245,27 @@ void rotacion(string ImageFile, string OutputFile, double gr){
 	 	ofstream pOutFile;
 		pOutFile.open(OutputFile, ios::out | ios::trunc | ios::binary);
     	if(pOutFile.is_open()){
-		//Se escribe la cabecera 
-   		unsigned char cabecera[8];
-   		InFile.read((char*)& cabecera, 8);
-   		pOutFile.write( (char *)& cabecera, 8);	
-   		//Se vuelca la matriz en el vector
-		vector<unsigned char> imgdata(matrix_size);
-		InFile.read((char*)& imgdata[0], matrix_size);
-			
-			
+		unsigned char cabecera[8];
+   		InFile.read((char*)& cabecera, 8); //Se lee la cabecera
+   		pOutFile.write( (char *)& cabecera, 8);	 //Se escribe la cabecera
+   		vector<unsigned char> imgdata(matrix_size); 
+		InFile.read((char*)& imgdata[0], matrix_size); //Se vuelca la matriz en el vector
 		InFile.close();
-		vector<unsigned char> fin(matrix_size);
+		vector<unsigned char> fin(matrix_size);  //Vector donde almacenaremos las matrices RGB rotadas
 		double xc,yc,xi,yi;
 		int xf,yf;
-		//Calculamos el centro de la imagen
-		xc=WIDTH/2;
+		xc=WIDTH/2; //Calculamos el centro de la imagen
 		yc=HEIGHT/2;
 		int contador=0;
 		int offset=0;
-		for(int k=0; k<3; ++k){
+		for(int k=0; k<3; ++k){ //Se aplica una rotacion por cada color
 			for (int j=0; j<HEIGHT; ++j){
 				for(int i=0; i<WIDTH; ++i){
 					xi=i-xc;
 					yi=j-yc;
-					xf= ceil( cos((gr*M_PI)/180)*xi - sin((gr*M_PI)/180)*yi +xc);
+					xf= ceil( cos((gr*M_PI)/180)*xi - sin((gr*M_PI)/180)*yi +xc); //Formula para calcular las posiciones resultantes de la rotacion
 					yf= ceil( sin((gr*M_PI)/180)*xi + cos((gr*M_PI)/180)*yi +yc);
-					if(yf<HEIGHT && yf>=0 && xf<WIDTH && xf>=0){
+					if(yf<HEIGHT && yf>=0 && xf<WIDTH && xf>=0){ //En el caso de que este dentro de las dimensiones/lienzo de la matriz original se escribe
 						fin[(yf*WIDTH + xf)+offset]= imgdata[contador];
 						
 					}
@@ -284,8 +274,8 @@ void rotacion(string ImageFile, string OutputFile, double gr){
 			}
 			offset+=HEIGHT*WIDTH;
 		}
-      		pOutFile.write((char*)& fin[0], matrix_size);
-	   		pOutFile.close();
+      		pOutFile.write((char*)& fin[0], matrix_size); //Se escribe las matrices rotadas en el fichero de salida
+	   	pOutFile.close();
 	   	}else{
 			cerr<<"Error al abrir "<<OutputFile<<endl;
 	   	}
@@ -293,48 +283,44 @@ void rotacion(string ImageFile, string OutputFile, double gr){
 		cerr<<"Error al abrir "<<ImageFile<<endl;
 }  
 }
-
 void MaxMin(string ImageFile, string OutputFile){
 	ifstream InFile;
 	InFile.open(ImageFile, ios::in | ios::binary);
  	if (InFile.is_open()) {
-		int colores[]= {0,255,0,255,0,255} ;
+		int colores[]= {0,255,0,255,0,255} ; //Valores iniciales
 		vector<unsigned char> imgdata(matrix_size); //Vector para volcar la matriz recibida
-		//No leemos los bytes que indican el tamaÃ±o de la matriz
-		InFile.seekg(8);
-		InFile.read((char*) &imgdata[0], matrix_size);
+		InFile.seekg(8); //No se leen los bytes que indican el tamaño de la matriz
+		InFile.read((char*) &imgdata[0], matrix_size); //Se lee la matriz 
 		InFile.close();
 		int i =0;
-		for (; i < WIDTH*HEIGHT; ++i){
-			//Hay un nuevo maximo
-			if (colores[0] < imgdata[i]){
+		for (; i < WIDTH*HEIGHT; ++i){ //Para la matriz del color rojo
+			if (colores[0] < imgdata[i]){ //Hay un nuevo maximo
 				colores[0]=imgdata[i];
 			}
-			//Hay un nuevo minimo
-			if (colores[1] > imgdata[i]){
+			if (colores[1] > imgdata[i]){ //Hay un nuevo minimo
 				colores[1]=imgdata[i];
 			}
 		}
-		for (; i < WIDTH*HEIGHT*2; ++i){
-			if (colores[2] < imgdata[i]){
+		for (; i < WIDTH*HEIGHT*2; ++i){ //Para la matriz del color verde
+			if (colores[2] < imgdata[i]){ //Hay un nuevo maximo
 				colores[2]=imgdata[i];
 			}
-			if (colores[3] > imgdata[i]){
+			if (colores[3] > imgdata[i]){ //Hay un nuevo minimo
 				colores[3]=imgdata[i];
 			}
 		}
-		for (; i < matrix_size; ++i){
-			if (colores[4] < imgdata[i]){
+		for (; i < matrix_size; ++i){ //Para la matriz del color azul
+			if (colores[4] < imgdata[i]){ //Hay un nuevo maximo
 				colores[4]=imgdata[i];
 			}
-			if (colores[5] > imgdata[i]){
+			if (colores[5] > imgdata[i]){ //Hay un nuevo minimo
 				colores[5]=imgdata[i];
 			}
 		}
 		ofstream pOutFile;
 		pOutFile.open(OutputFile, ios::out | ios::trunc | ios::binary);
 		if(pOutFile) { 
-			for (int i=0; i<6; i++){
+			for (int i=0; i<6; i++){ //Se escriben los resultados en el fichero de salida
 				pOutFile<<colores[i];
 				if(i<5){
 					pOutFile<<" ";
@@ -348,29 +334,27 @@ void MaxMin(string ImageFile, string OutputFile){
 	 	cerr<<"Error al abrir el fichero "<<ImageFile<<endl;
 	}
 }
-
 void aplicar_filtro(string ImageFile, string OutputFile, double r){
 	ifstream InImagen;
 	InImagen.open(ImageFile, ios::in | ios::binary);
 	if (InImagen.is_open()){
 		streampos fileSize= matrix_size+8;
 		vector<unsigned char> imgdata(fileSize); //Vector para volcar la matriz recibida
-		InImagen.read((char*) &imgdata[0], fileSize);
+		InImagen.read((char*) &imgdata[0], fileSize); //Se lee toda la imagen
 		InImagen.close();
 		struct punto{
 			double x;
 			double y;
 		};
 		struct punto centro, p;
-		centro.x=WIDTH/2;
+		centro.x=WIDTH/2; //Se calcula el centro de la imagen
 		centro.y=HEIGHT/2;
 		int i, j;
 		for(j =0; j<HEIGHT; ++j){
 			for(i=0; i<WIDTH; ++i){
 				p.x= i-centro.x;
 				p.y= j-centro.y;
-				//Si esta fuera del radio, lo cambio de color
-				if (p.x * p.x + p.y * p.y > r*r){
+				if (p.x * p.x + p.y * p.y > r*r){ //Si esta fuera del radio, se cambia de color
 					imgdata[i+j*WIDTH+8] = floor(imgdata[i+j*WIDTH+8]*0.3);	
 					imgdata[i+WIDTH*(j+HEIGHT)+8] = floor(imgdata[i+WIDTH*(j+HEIGHT)+8]*0.59);
 					imgdata[i+WIDTH*(j+HEIGHT*2)+8] = floor(imgdata[i+WIDTH*(j+HEIGHT*2)+8]*0.11);
@@ -380,7 +364,7 @@ void aplicar_filtro(string ImageFile, string OutputFile, double r){
 		ofstream OutFile;
 		OutFile.open(OutputFile, ios::out | ios::trunc | ios::binary);
 		if(OutFile.is_open()) {
-			OutFile.write((char*)& imgdata[0], 1);
+			OutFile.write((char*)& imgdata[0], 1); //Se escribe la matriz resultante en el fichero de salida
 			OutFile.close();
 		}
 		else{
